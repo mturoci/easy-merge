43baa3abc5a47209b18ed825c5135a1ee38b0975
Martin Turoci <martinturoci@gmail.com>
2022-02-25 15:13:58 +0100

    feat: Add finish command

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -7,6 +7,8 @@ import * as interfaces from './interfaces'
 import MergeDecorator from './decorator'
 import ContentProvider from './contentProvider'
 import store from './store'
+import { promisify } from "util"
+import { exec } from "child_process"
 
 interface IDocumentMergeConflictNavigationResults {
   canNavigate: boolean
@@ -18,6 +20,8 @@ enum NavigationDirection {
   Backwards
 }
 
+const modalWarning = 'The file contains unresoved conflicts. Do you still want to mark merge as resolved and stage the changes?'
+const promiseExec = promisify(exec)
 export default class CommandHandler implements vscode.Disposable {
 
   private disposables: vscode.Disposable[] = [];
@@ -32,6 +36,7 @@ export default class CommandHandler implements vscode.Disposable {
   begin() {
     this.context.subscriptions.push(vscode.commands.registerCommand('simple-merge.diff', this.diff.bind(this)))
     this.context.subscriptions.push(vscode.commands.registerCommand('simple-merge.accept', this.accept.bind(this)))
+    this.context.subscriptions.push(vscode.commands.registerCommand('simple-merge.finish', this.finish.bind(this)))
     this.decorator.begin()
   }
 
@@ -44,13 +49,13 @@ export default class CommandHandler implements vscode.Disposable {
       return
     }
 
+    await vscode.commands.executeCommand('setContext', 'simple-merge.diffing', true)
     let ignoreRanges = conflicts.map(c => [c.incoming.content, c.range])
     const leftUri = document.uri.with({ scheme: ContentProvider.schemeCurrent, query: JSON.stringify({ ranges: ignoreRanges }) })
 
     ignoreRanges = conflicts.map(c => [c.current.content, c.range])
     const rightUri = leftUri.with({ scheme: ContentProvider.schemeIncoming, query: JSON.stringify({ ranges: ignoreRanges }) })
 
-    await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
     await vscode.commands.executeCommand('vscode.openWith', leftUri, 'default')
     await vscode.commands.executeCommand('vscode.openWith', document.uri, 'default', vscode.ViewColumn.Beside)
     await vscode.commands.executeCommand('vscode.openWith', rightUri, 'default', vscode.ViewColumn.Beside)
@@ -60,6 +65,22 @@ export default class CommandHandler implements vscode.Disposable {
 
     this.decorator.applyDecorations(currentEditor, conflicts, 'current')
     this.decorator.applyDecorations(incomingEditor, conflicts, 'incoming')
+  }
+
+  async finish(uri: vscode.Uri) {
+    const document = await vscode.workspace.openTextDocument(uri)
+    const conflicts = await this.tracker.getConflicts(document)
+
+    if (conflicts) {
+      const answer = await vscode.window.showWarningMessage(modalWarning, { modal: true }, "Yes")
+      if (answer !== 'Yes') return
+    }
+
+    await promiseExec(`git add ${uri.fsPath}`, { cwd: vscode.workspace.workspaceFolders![0].uri.fsPath })
+    await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+    await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+    await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+    await vscode.commands.executeCommand('setContext', 'simple-merge.diffing', false)
   }
 
   private async accept(conflict: interfaces.IDocumentMergeConflict, commitType: interfaces.CommitType) {
